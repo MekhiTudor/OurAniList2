21334ec526267bb4a770abcdc28bcaa2e7edbd3e
MekhiTudor <164079314+MekhiTudor@users.noreply.github.com>
2024-09-05 15:13:34 -0400

    trying this bs again

--- server/index.js
+++ server/index.js
@@ -4,13 +4,7 @@ const path = require("path");
 const pathToDist = path.join(__dirname, "../AniList/dist");
 const serveStatic = express.static(pathToDist);
 
-const sayHello = (req, res, nxt) => {
-  console.log(req);
-  res.send("hello");
-};
-
 app.use(serveStatic);
 
-app.get("/", sayHello);
 const port = 9999;
 app.listen(port, () => console.log(`listening at http://localhost:${port}`));
