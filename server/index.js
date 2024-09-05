fa552acf79a4d432f8572d5247a4bec884c3228e
MekhiTudor <164079314+MekhiTudor@users.noreply.github.com>
2024-09-05 14:05:56 -0400

    adding server

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const pathToDist = path.join(__dirname, "../OurAniList2/AniList/dist");
+const serveStatic = express.static(pathToDist);
+
+app.use(serveStatic);
+const port = 9999;
+app.listen(port, () => console.log(`listening at http://localhost:${port}`));
