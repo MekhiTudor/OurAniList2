f19e9e96f91c96d083bf8fb79b482daefb41f04e
MekhiTudor <164079314+MekhiTudor@users.noreply.github.com>
2024-09-06 14:49:35 -0400

    updating

--- server/index.js
+++ server/index.js
@@ -5,6 +5,6 @@ const pathToDist = path.join(__dirname, "../AniList/dist");
 const serveStatic = express.static(pathToDist);
 
 app.use(serveStatic);
-
+app.get("/", () => console.log("i work"));
 const port = 1234;
 app.listen(port, () => console.log(`listening at http://localhost:${port}`));
